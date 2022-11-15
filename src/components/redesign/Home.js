1b8f7218f2fa4267a431b549c97ca77bfafb6a15
davekempsell <dave_kempsell@hotmail.com>
2022-11-15 14:07:14 +0000

    more info section added to mobile view only, with some restyling of text and images

--- src/components/redesign/Home.js
+++ src/components/redesign/Home.js
@@ -14,6 +14,18 @@ export function Home() {
           </div>
           <img src='/images/bitmoji-head.png' alt="daves head"></img>
         </div>
+        <div className="more-info-container">
+          <div className="devices-images">
+            <img id="devices-image" src='/images/devices-transparent.png' alt="black and white, various screens"></img>
+          </div>
+          <div className="more-info-content">
+            <div className="more-info-text">
+              <p>Some stuff about my career and journey, etc etc. Ideally this will be a couple of sentences.</p>
+              <p>List out some reasons why I'd be a great person to hire, but keep it kind of humble brag style.</p>
+              <p>And finally something else to finish off this section.</p>
+            </div>
+          </div>
+        </div>
       </div>
       {Filler()}
     </div>
