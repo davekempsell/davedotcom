d0892e135b518fb62a7d1321f4e8d64ecc3323e7
davekempsell <dave_kempsell@hotmail.com>
2022-11-15 20:55:46 +0000

    project section added

--- src/components/redesign/Home.js
+++ src/components/redesign/Home.js
@@ -13,6 +13,26 @@ export function Home() {
         {Introduction()}
         {MoreInfo()}
         {Experience()}
+        <div className="projects-container">
+          <div className="projects-text">
+            <h3>Recent Projects</h3>
+            <p>Here are some of the projects I've been working on recently:</p>
+          </div>
+          <div className="project-cards-container">
+            <div className="project-card">
+              <h3>Project Name</h3>
+            </div>
+            <div className="project-card">
+              <h3>Project Name</h3>
+            </div>
+            <div className="project-card">
+              <h3>Project Name</h3>
+            </div>
+            <div className="project-card">
+              <h3>Project Name</h3>
+            </div>
+          </div>
+        </div>
       </div>
       {Filler()}
     </div>
