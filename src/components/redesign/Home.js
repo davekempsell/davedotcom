3da1f6910c9e99f5d0730534c3818a0b47df4ac3
davekempsell <dave_kempsell@hotmail.com>
2022-11-16 11:33:30 +0000

    Projects section completed and styled. links inlcuded on cards to site/github

--- src/components/redesign/Home.js
+++ src/components/redesign/Home.js
@@ -4,6 +4,7 @@ import NavBar from './NavBar';
 import Introduction from './Introduction';
 import MoreInfo from './MoreInfo';
 import Experience from "./Experience";
+import Projects from './Projects';
 
 export function Home() {
   return (
@@ -13,26 +14,7 @@ export function Home() {
         {Introduction()}
         {MoreInfo()}
         {Experience()}
-        <div className="projects-container">
-          <div className="projects-text">
-            <h3>Projects</h3>
-            <p>Here are some of the projects I've been working on recently:</p>
-          </div>
-          <div className="project-cards-container">
-            <div className="project-card">
-              <h3>Project Name</h3>
-            </div>
-            <div className="project-card">
-              <h3>Project Name</h3>
-            </div>
-            <div className="project-card">
-              <h3>Project Name</h3>
-            </div>
-            <div className="project-card">
-              <h3>Project Name</h3>
-            </div>
-          </div>
-        </div>
+        {Projects()}
       </div>
       {Filler()}
     </div>
