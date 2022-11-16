2bb6bf74c352df78a1031ac8fd9c87c8d1526423
davekempsell <dave_kempsell@hotmail.com>
2022-11-16 14:43:30 +0000

    mobile navbar fixed at top of page

--- src/components/redesign/NavBar.js
+++ src/components/redesign/NavBar.js
@@ -1,31 +1,23 @@
-import { useState } from 'react';
-
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faNavicon, faEnvelope } from "@fortawesome/free-solid-svg-icons";
+import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
 import { faGithub, faLinkedin } from "@fortawesome/free-brands-svg-icons";
 import '../../NewApp.css'
 
 export function NavBar() {
-  const [naviconsState, setNaviconsState] = useState(false)
-
-  function showNavicons() {
-    if(naviconsState) {
-      return (
-        <div className="navicons mobile">
-          <a href='https://github.com/davekempsell' target="_blank" rel="noreferrer">
-            <FontAwesomeIcon icon={faGithub} />
-          </a>
-          <a href='https://www.linkedin.com/in/dave-kempsell/' target="_blank" rel="noreferrer">
-            <FontAwesomeIcon icon={faLinkedin} />
-          </a>
-          <a href="mailto: davekempsell87@gmail.com" target="_blank" rel="noreferrer">
-            <FontAwesomeIcon icon={faEnvelope} />
-          </a>
-        </div>
-      )
-    } else {
-      return null;
-    }
+  const navicons = view => {
+    return (
+      <div className={`navicons ${view}`}>
+        <a href='https://github.com/davekempsell' target="_blank" rel="noreferrer">
+          <FontAwesomeIcon icon={faGithub} />
+        </a>
+        <a href='https://www.linkedin.com/in/dave-kempsell/' target="_blank" rel="noreferrer">
+          <FontAwesomeIcon icon={faLinkedin} />
+        </a>
+        <a href="mailto: davekempsell87@gmail.com" target="_blank" rel="noreferrer">
+          <FontAwesomeIcon icon={faEnvelope} />
+        </a>
+      </div>
+    )
   }
 
 
@@ -33,27 +25,8 @@ export function NavBar() {
     <div>
       <div className="nav-container">
         <img src='/images/favicon.ico' alt="glasses logo"></img>
-        <button className="navicon">
-          <FontAwesomeIcon id="navicon" icon={faNavicon} onClick={() => {
-            if(naviconsState) {
-              setNaviconsState(false)
-            } else {
-              setNaviconsState(true)
-            }
-          }}/>
-        </button>
-        <div className="navicons desktop">
-          <a href='https://github.com/davekempsell' target="_blank" rel="noreferrer">
-            <FontAwesomeIcon icon={faGithub} />
-          </a>
-          <a href='https://www.linkedin.com/in/dave-kempsell/' target="_blank" rel="noreferrer">
-            <FontAwesomeIcon icon={faLinkedin} />
-          </a>
-          <a href="mailto: davekempsell87@gmail.com" target="_blank" rel="noreferrer">
-            <FontAwesomeIcon icon={faEnvelope} />
-          </a>
-        </div>
-        {showNavicons()}
+        {navicons('mobile')}
+        {navicons('desktop')}
       </div>
     </div>
   )
